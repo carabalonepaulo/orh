1d760f2be8bf8aff497f2f4456e9b160845bb2fb
Paulo Carbalone <psoreto@outlook.com>
2022-07-09 15:15:22 -0300

    using ms instead of us

--- src/app.gd
+++ src/app.gd
@@ -8,13 +8,13 @@ func start() -> void:
     app = App.new()
 
     app.use(func(ctx: Dictionary) -> bool:
-        ctx["_log_start"] = Time.get_ticks_usec()
+        ctx._log_start = Time.get_ticks_msec()
         return true)
     app.use(get_router())
     app.use(func(ctx: Dictionary) -> bool:
-        var elapsed: float = Time.get_ticks_usec() - ctx["_log_start"]
+        var elapsed: float = Time.get_ticks_msec() - ctx._log_start
         var now := Time.get_datetime_dict_from_system()
-        var formatted := "[%s:%s:%s %s/%s/%s] %s %s %dus" % [now["hour"],
+        var formatted := "[%s:%s:%s %s/%s/%s] %s %s %dms" % [now["hour"],
             now["minute"], now["second"], now["month"], now["day"],
             now["year"], ctx.req.method, ctx.req.uri.path, elapsed]
         print(formatted)
