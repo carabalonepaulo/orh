a64c5867c3b66deadcc242988dbb0c715613e9c1
Paulo Carbalone <psoreto@outlook.com>
2022-07-09 15:08:29 -0300

    new app example

--- src/app.gd
+++ src/app.gd
@@ -1,7 +1,37 @@
-extends App
+extends Main
 
 
-func create_router() -> Router:
+var app: App
+
+
+func start() -> void:
+    app = App.new()
+
+    app.use(func(ctx: Dictionary) -> bool:
+        ctx["_log_start"] = Time.get_ticks_usec()
+        return true)
+    app.use(get_router())
+    app.use(func(ctx: Dictionary) -> bool:
+        var elapsed: float = Time.get_ticks_usec() - ctx["_log_start"]
+        var now := Time.get_datetime_dict_from_system()
+        var formatted := "[%s:%s:%s %s/%s/%s] %s %s %dus" % [now["hour"],
+            now["minute"], now["second"], now["month"], now["day"],
+            now["year"], ctx.req.method, ctx.req.uri.path, elapsed]
+        print(formatted)
+        return true)
+
+    app.start()
+
+
+func stop() -> void:
+    app.stop()
+
+
+func update() -> void:
+    app.update()
+
+
+func get_router() -> Router:
     var router := Router.new()
 
     router.use("GET", "/", func(ctx: Dictionary):
@@ -11,10 +41,3 @@ func create_router() -> Router:
         ctx.res.send(200, "nominho: %s" % ctx.params.name))
 
     return router
-
-
-func get_middlewares() -> Array:
-    return [
-        Logger.new(),
-        create_router()
-    ]
