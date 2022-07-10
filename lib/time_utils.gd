b55d722ede8c31c5d3e4d3614175a5b1d3f15e9e
Paulo Carbalone <psoreto@outlook.com>
2022-07-10 02:06:25 -0300

    time utils added

---
+++ lib/time_utils.gd
@@ -0,0 +1,13 @@
+class_name TimeUtils
+
+
+static func days(num: int) -> int:
+    return num * 60 * 60 * 24
+
+
+static func hours(num: int) -> int:
+    return num * 60 * 60
+
+
+static func minutes(num: int) -> int:
+    return num * 60
